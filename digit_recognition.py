6a57171b223c159855fd09acb7e6e20fb993560b
sibiraaghav <134054179+sibiraaghav@users.noreply.github.com>
2023-11-09 13:05:00 +0530

    Add files via upload
    Digit recognition done with the help of MNIST dataset using python.

---
+++ digit_recognition.py
@@ -0,0 +1,49 @@
+#Bharat Intern Task 3
+#Handwritten Digit Recognition using MNIST dataset
+
+import os
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf
+
+#Splitting into training and testing data
+mnist = tf.keras.datasets.mnist
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train = tf.keras.utils.normalize(x_train, axis=1)
+x_test = tf.keras.utils.normalize(x_test, axis=1)
+
+#Building the model
+model = tf.keras.models.Sequential()
+model.add(tf.keras.layers.Flatten(input_shape=(28, 28)))
+model.add(tf.keras.layers.Dense(128, activation='relu'))
+model.add(tf.keras.layers.Dense(128, activation='relu'))
+model.add(tf.keras.layers.Dense(10, activation='softmax'))
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+#Fitting the model
+model.fit(x_train, y_train, epochs=3)
+
+#Accuracy metrics
+loss,accuracy = model.evaluate(x_test, y_test)
+print(loss)
+print(accuracy)
+
+model.save('recognition.model')
+
+#Loading the model and predictions using handwritten digits
+model = tf.keras.models.load_model('recognition.model')
+image_no = 1
+while os.path.isfile(f"digits/digit{image_no}.png"):
+    try:
+        img = cv2.imread(f"digits/digit{image_no}.png")[:,:,0]
+        img = np.invert(np.array([img]))
+        prediction = model.predict(img)
+        print(f"The digit could be {np.argmax(prediction)}")
+        plt.imshow(img[0], cmap=plt.cm.binary)
+        plt.show()
+    except:
+        print("Error")
+    finally:
+        image_no += 1
+
